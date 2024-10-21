83d36813595c2bfd3cda511dd0dc4df638128b22
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2024-10-21 12:16:11 +0300

    Update module_3_5_corrected_1.py.py

--- module_3_5.corrected.py.py
+++ module_3_5.corrected.py.py
@@ -1,11 +1,13 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if not str_number:
-        return 1
     first = int(str_number[0])
     if first == 0:
-        return get_multiplied_digits(str_number[1:])
-    return first * get_multiplied_digits(str_number[1:])
+        return get_multiplied_digits(int(str_number[1:])) if len(str_number) > 1 else 1
 
-result = get_multiplied_digits(420)
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
 print(result)
