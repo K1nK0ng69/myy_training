3dc999dba2d89457af143e6e5c6ab39d818e2b9d
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2024-11-26 16:50:33 +0300

    Update module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -1,16 +1,12 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-# Список длин строк из first_strings, если длина строки не менее 5 символов
 first_result = [len(s) for s in first_strings if len(s) >= 5]
 
-# Список пар (кортежей) слов одинаковой длины из first_strings и second_strings
 second_result = [(s1, s2) for s1 in first_strings for s2 in second_strings if len(s1) == len(s2)]
 
-# Словарь строк и их длин, если длина строки четная, из объединения списков
 third_result = {s: len(s) for s in first_strings + second_strings if len(s) % 2 == 0}
 
-# Вывод результатов
 print(first_result)
 print(second_result)
 print(third_result)
