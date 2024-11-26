609dfe75ef860e8c688cee989c73c9109863b29e
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2024-11-26 16:50:54 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,12 +1,9 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-# Генераторная сборка для вычисления разницы длин, если длины строк не равны
 first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
 
-# Генераторная сборка для сравнения длин строк без использования zip
 second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
-# Пример вывода результатов
-print(list(first_result))  # Генератор разворачивается в список для просмотра результата
+print(list(first_result)) 
 print(list(second_result))
