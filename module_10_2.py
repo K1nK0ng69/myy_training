9fcb5e371d2703b0f2f38e1cb177cc27727ff6f7
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2024-12-03 11:19:52 +0300

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -1,7 +1,6 @@
 from threading import Thread
 from time import sleep
 
-# Общая переменная для врагов
 total_enemies = 100
 
 
@@ -19,23 +18,20 @@ class Knight(Thread):
         while total_enemies > 0:
             days += 1
             sleep(1)
-            if total_enemies > 0:  # Проверка, остались ли враги
+            if total_enemies > 0:
                 total_enemies -= self.power
-                total_enemies = max(total_enemies, 0)  # Чтобы врагов не стало меньше 0
+                total_enemies = max(total_enemies, 0)
                 print(f"{self.name} сражается {days} день(дня)..., осталось {total_enemies} воинов.")
 
         print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
 
-# Создание рыцарей
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight('Sir Galahad', 20)
 
-# Запуск потоков
 first_knight.start()
 second_knight.start()
 
-# Ожидание завершения потоков
 first_knight.join()
 second_knight.join()
 
