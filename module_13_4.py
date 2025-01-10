f62e59ffa987d1cc3e0f742cf3e5dc6185a44ae5
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2025-01-10 21:26:10 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+from aiogram.types import Message
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+API_TOKEN = ""
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(Text(equals='Calories', ignore_case=True))
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    try:
+        age = int(data['age'])
+        growth = int(data['growth'])
+        weight = int(data['weight'])
+
+        calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+        await message.answer(f"Ваша норма калорий: {calories} ккал в день.")
+    except ValueError:
+        await message.answer("Пожалуйста, вводите только числа для возраста, роста и веса.")
+
+    await state.finish()
+
+
+if __name__ == "__main__":
+    print("Бот запущен. Ожидаем сообщения...")
+    executor.start_polling(dp, skip_updates=True)
