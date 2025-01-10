3cd8b17479d12620bec8586c023fdb77b79081a3
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2025-01-10 21:22:41 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,22 @@
+
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+
+API_TOKEN = ""
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+# Обработчик всех остальных сообщений
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    print("Бот запущен. Ожидаем сообщения...")
+    executor.start_polling(dp, skip_updates=True)
