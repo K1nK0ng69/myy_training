ec06cd420985906b7d53fab2b599a4c5e47783cc
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2025-01-16 10:05:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while a<len(my_list):
-    number = my_list[a]
-    if number > 0:
-        print(number)
-    elif number < 0:
-        break;
-    a += 1
+# 1st program
+print(9 ** 0.5 * 5)
 
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
 
+# 3rd program
+result_without_priority = 2 * 2 + 2
+result_with_priority = 2 * (2 + 2)
+# cравнение двух результатов
+print(result_without_priority)
+print(result_with_priority)
+print(result_without_priority == result_with_priority)
+
+# 4th program
+number_str = '123.456'
+number_float = float(number_str)
+shifted_number = number_float * 10
+shifted_int = int(shifted_number)
+first_decimal_digit = shifted_int % 10
+print(first_decimal_digit)
