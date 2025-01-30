beead2e1f75a045be16f902438e653bcbcb2380a
K1nK0ng69 <152622976+K1nK0ng69@users.noreply.github.com>
2025-01-30 14:00:27 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -11,6 +11,24 @@ def initiate_db():
             price INTEGER NOT NULL
         )
     ''')
+    cursor.execute('''
+          CREATE TABLE IF NOT EXISTS Users (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT NOT NULL UNIQUE,
+              email TEXT NOT NULL,
+              age INTEGER NOT NULL,
+              balance INTEGER NOT NULL DEFAULT 1000
+          )
+      ''')
+    conn.commit()
+    conn.close()
+
+def add_user(username, email, age):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Users (username, email, age) VALUES (?, ?, ?)
+    ''', (username, email, age))
     conn.commit()
     conn.close()
 
@@ -28,3 +46,11 @@ def add_product(title, description, price):
     cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)', (title, description, price))
     conn.commit()
     conn.close()
+
+def is_included(username):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
